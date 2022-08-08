80d59c31e4f5c442d3c75645558a7d322c077d19
Luisops <luisjeremias33.3@gmail.com>
2022-08-08 00:54:25 -0500

    Iniciando la configuracion del proyecto web con maven y spring MVC

---
+++ src/main/java/com/luisops/capitulo2/SaludoController.java
@@ -0,0 +1,13 @@
+package com.luisops.capitulo2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class SaludoController {
+	
+	@RequestMapping("saludo")
+	public String saludar() {
+		return "saludo.jsp";
+	}
+}
